eaa061eee341c9b9b6a008f2a5c56492a3ae50aa
fanliangandrew <986406153@qq.com>
2016-04-10 17:58:04 +0800

    all commit git commit -m "all" $git commit -m "all"

---
+++ test/DatabaseUtilsTest.java
@@ -0,0 +1,24 @@
+package wheellllll.database;
+
+import org.junit.Test;
+
+/**
+ * This class is written for testing database
+ */
+public class DatabaseUtilsTest{
+
+    @Test
+    public void testIsExisted() throws Exception {
+
+    }
+
+    @Test
+    public void testIsValid() throws Exception {
+
+    }
+
+    @Test
+    public void testCreateAccount() throws Exception {
+
+    }
+}
